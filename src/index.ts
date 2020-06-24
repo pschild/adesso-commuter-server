d3047fa58e3e4211935cdf83016dd1f3e043a0f4
Philippe Schild <pschild@users.noreply.github.com>
2020-06-24 07:49:42 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ import * as fs from 'fs';
 const app: Application = express();
 const port = 9046;
 
+const ADESSO_ESSEN = [51.4557381, 7.0101814];
+const UNI_ESSEN = [51.4649085, 7.0014287];
+const AUFFAHRT_A42 = [51.5045685, 6.9971393];
+const AUFFAHRT_A40 = [51.4471093, 7.0011169];
+const KA_LI = [51.4914203, 6.5759749];
+const GOCH = [51.668189, 6.148282];
+
+// Example: https://www.google.de/maps/dir/51.5045685,6.9971393/51.668189,6.148282
+
 app.get('/', (req, res) => {
   const content = fs.readFileSync('history.log');
   res.status(200).send(content);
