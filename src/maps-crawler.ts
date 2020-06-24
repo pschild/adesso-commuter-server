dc740b2a4b998e9955009b3272cb6a8680385e8d
Philippe Schild <pschild@users.noreply.github.com>
2020-06-24 23:18:51 +0200

    Update maps-crawler.ts

--- src/maps-crawler.ts
+++ src/maps-crawler.ts
@@ -5,7 +5,8 @@ export class GoogleMapsCrawler {
 
   async crawl(origin: LatLng, destination: LatLng): Promise<number[]> {
     // const browser = await puppeteer.launch({ headless: true });
-    const browser = await puppeteer.launch({ executablePath: process.env.CHROMIUM_PATH, headless: true, args: ['--no-sandbox'] });
+    // const browser = await puppeteer.launch({ executablePath: process.env.CHROMIUM_PATH, headless: true, args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ executablePath: 'chromium-browser' });
     const page = await browser.newPage();
     await page.goto(
       `https://www.google.de/maps/dir/${origin.latitude},${origin.longitude}/${destination.latitude},${destination.longitude}`
